fbec30905c74933c0ab8b2f7314a738cd7ea45e2
SagarBarua1010 <sagarbarua1010@gmail.com>
2024-08-21 17:15:54 +0600

    Shopping cart

---
+++ src/Components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ReviewItem.css';
+
+
+const ReviewItem = ({product}) => {
+    const {id,img,price,quantity,name} = product;
+    return (
+        <div className='review-item'>
+           <img src={img} alt="" />
+        </div>
+    );
+};
+
+export default ReviewItem;
