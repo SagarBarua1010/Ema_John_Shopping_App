e5ef95cefa8f4af74e740811060a48dff7a20977
SagarBarua1010 <sagarbarua1010@gmail.com>
2024-08-21 19:40:17 +0600

    remove

--- src/Components/ReviewItem/ReviewItem.jsx
+++ src/Components/ReviewItem/ReviewItem.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-
-const ReviewItem = ({product}) => {
+const ReviewItem = ({product,handleRemoveFromCart}) => {
     const {id,img,price,quantity,name} = product;
     return (
         <div className='review-item'>
            <img src={img} alt="" />
+           <div className='review-details'>
+            <p className='product-title'>{name}</p>
+            <p>price:<samp className='price-text'>${price}</samp></p>
+            <p>Quantity:<samp className='price-text'>${quantity}</samp></p>
+           </div>
+           <button className='btn-delete'>
+           <FontAwesomeIcon onClick={()=> handleRemoveFromCart(id)} className='delete-icon' icon=  { faTrashAlt} />
+           </button>
         </div>
     );
 };
