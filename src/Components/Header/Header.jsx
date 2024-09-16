20b73481969e1054ef689eeddb54d18f49ac6583
SagarBarua1010 <sagarbarua1010@gmail.com>
2024-09-17 01:19:45 +0600

    sign out

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,19 @@
-
-
-
 import './Header.css';
 import logo from '../../images/Logo.svg';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
+import { useContext } from 'react';
 
 
 const Header = () => {
+    const {user, logOut} = useContext(AuthContext)
+    console.log(user)
+    const handleLogout = () =>{
+        logOut()
+        .then(result =>{})
+        .catch(error=>console.error(error));
+
+    }
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -16,6 +23,7 @@ const Header = () => {
             <Link to="inventory">Inventory</Link>
             <Link to="login">Login</Link>
             <Link to="/signup">Sign Up</Link>
+            {user && <span>Welcome {user.email} <button onClick={handleLogout}>Sing out</button> </span>}
             </div>
         </nav>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,55 @@
-import React from 'react'
-import * as ReactDOM from "react-dom/client"
-import './index.css'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import Shop from './Components/Shop/Shop.jsx'
-import Home from './Components/Layout/Home.jsx'
-import Orders from './Components/Orders/Orders.jsx'
-import Inventory from './Components/Inventory/Inventory.jsx'
-import Login from './Components/Login/Login.jsx'
-import cartProductsLoader from './CartProductsLoader/CartProductsLoader.js'
-import Checkout from './Components/Checkout/Checkout.jsx'
-import SignUp from './Components/SignUp/SignUp.jsx';
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Shop from "./Components/Shop/Shop.jsx";
+import Home from "./Components/Layout/Home.jsx";
+import Orders from "./Components/Orders/Orders.jsx";
+import Inventory from "./Components/Inventory/Inventory.jsx";
+import Login from "./Components/Login/Login.jsx";
+import cartProductsLoader from "./CartProductsLoader/CartProductsLoader.js";
+import Checkout from "./Components/Checkout/Checkout.jsx";
+import SignUp from "./Components/SignUp/SignUp.jsx";
+import AuthProvider from "./Components/Provider/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
-    path:'/',element:<Home></Home>,
-    children:[
+    path: "/",
+    element: <Home></Home>,
+    children: [
       {
-        path:'/',element:<Shop></Shop>
+        path: "/",
+        element: <Shop></Shop>,
       },
       {
-        path:'orders',element:<Orders></Orders>,
-        loader:cartProductsLoader
+        path: "orders",
+        element: <Orders></Orders>,
+        loader: cartProductsLoader,
       },
       {
-        path:'inventory',element:<Inventory></Inventory>
+        path: "inventory",
+        element: <Inventory></Inventory>,
       },
       {
-        path:'checkout',element:<Checkout></Checkout>
+        path: "checkout",
+        element: <Checkout></Checkout>,
       },
       {
-        path:'login',element:<Login></Login>
+        path: "login",
+        element: <Login></Login>,
       },
       {
-        path: '/signup',element:<SignUp></SignUp>
-      }
-    ]
-  }
-])
+        path: "/signup",
+        element: <SignUp></SignUp>,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
